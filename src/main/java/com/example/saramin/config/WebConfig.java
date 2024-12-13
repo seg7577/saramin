3e917a5583546dc2c87c370be1ade999db22bfd4
seg7577 <seg082911@gmail.com>
2024-12-13 11:58:50 +0900

    로깅 설정 추가

---
+++ src/main/java/com/example/saramin/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.saramin.config;
+
+import com.example.saramin.logging.LoggingInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private LoggingInterceptor loggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
+}
