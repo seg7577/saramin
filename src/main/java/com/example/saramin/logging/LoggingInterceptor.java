9edd4c20ae3a30977c79c0534150860342ad0303
seg7577 <seg082911@gmail.com>
2024-12-13 11:52:56 +0900

    로깅 구현

--- src/main/java/com/example/saramin/logging/LoggingInterceptor.java
+++ src/main/java/com/example/saramin/logging/LoggingInterceptor.java
@@ -1,5 +1,27 @@
 package com.example.saramin.logging;
 
-public class LoggingInterceptor {
-    
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        logger.info("Request: {}", request.getRequestURL());
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception e) {
+        logger.info("Status: {}", response.getStatus());
+        if (e != null) {
+            logger.error("오류 발생: {}", e.getMessage());
+        }
+    }
 }
